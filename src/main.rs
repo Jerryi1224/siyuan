3bbd27b2a440bf9d41d4707c5f4e3f1bfd171a03
Jerryi1224 <35223411+Jerryi1224@users.noreply.github.com>
2024-05-18 16:49:44 +0800

    v0.1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use salvo::prelude::*;
 
 #[handler]
-async fn hello() -> &'static str {
-    "hello world"
+async fn hello(res: &mut Response) -> Result<&'static str, ()> {
+    Ok("hello world")
 }
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
     let router = Router::new().get(hello);
-    let accepter = TcpListener::new("127.0.0.1:5800").bind().await;
-    Server::new(accepter).serve(router).await;
+    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    Server::new(acceptor).serve(router).await;
 }
